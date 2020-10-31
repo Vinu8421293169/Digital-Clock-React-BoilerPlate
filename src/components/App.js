2e7fd245f00f8f6f125aec9887bcb7b6ca37a57e
Vinod Vyankatesh Gentyal <Vinod.gentyal@gmail.com>
2020-10-31 23:16:38 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,38 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component } from "react";
+import "../styles/App.css";
+
+let time = new Date();
 
 class App extends Component {
-    render() {
+  constructor() {
+    super();
+    this.state = {
+      hours: time.getHours(),
+      minutes: time.getMinutes(),
+      seconds: time.getSeconds()
+    };
+  }
 
-        return(
-            <>
-               
-            </>
-        )
-    }
+  render() {
+    return (
+      <div className="Clock">
+        <h3 id="time">
+          {this.state.hours}:{this.state.minutes}:{this.state.seconds}{" "}
+          {this.state.hours >= 12 ? "PM" : "AM"}
+        </h3>
+      </div>
+    );
+  }
+  componentDidMount() {
+    setInterval(() => {
+      let t = new Date();
+      this.setState({
+        hours: t.getHours(),
+        minutes: t.getMinutes(),
+        seconds: t.getSeconds()
+      });
+    }, 1000);
+  }
 }
-
 
 export default App;
