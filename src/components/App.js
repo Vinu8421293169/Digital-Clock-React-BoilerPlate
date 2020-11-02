09a1ecc86ad723b21163b6454e89f96c9aa0cbbf
Vinod Vyankatesh Gentyal <Vinod.gentyal@gmail.com>
2020-11-02 14:14:37 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,46 +1,45 @@
 import React, { Component } from "react";
 import "../styles/App.css";
 
-let time = new Date();
-
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      hours: time.getHours(),
-      minutes: time.getMinutes(),
-      seconds: time.getSeconds()
+      time: new Date().toLocaleTimeString("en-US", {
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric",
+        hour12: true
+      })
     };
   }
 
+  updateTime = () => {
+    this.setState({
+      time: new Date().toLocaleTimeString("en-US", {
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric",
+        hour12: true
+      })
+    });
+  };
+
   render() {
     return (
-      <div className="Clock">
-        <h3 id="time">
-          {this.state.hours}:{this.state.minutes}:{this.state.seconds}{" "}
-          {this.state.hours >= 12 ? "PM" : "AM"}
-        </h3>
+      <div className="App">
+        <div className="Clock">
+          <h3 id="time">{this.state.time}</h3>
+        </div>
       </div>
     );
   }
   componentDidMount() {
-    setInterval(() => {
-      this.setState({
-        seconds: (this.state.seconds + 1) % 60
-      });
-    }, 1000);
+    this.interval = setInterval(() => this.updateTime(), 1000);
+  }
 
-    setInterval(() => {
-      this.setState({
-        seconds: (this.state.minutes + 1) % 60
-      });
-    }, 60000);
-
-    setInterval(() => {
-      this.setState({
-        seconds: (this.state.hours + 1) % 60
-      });
-    }, 3600000);
+  componentWillUnmount() {
+    clearInterval(this.interval);
   }
 }
 
